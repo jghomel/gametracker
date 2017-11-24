c21b6ee0080bb0bdd2351290c906b056c2cd4c73
Jan-Philippe Lavoie <janphilippelavoie@gmail.com>
2017-11-24 14:37:29 -0500

    Added go page to display and choose the champion

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3
 MAINTAINER Jan-Philippe Lavoie "janphilippelavoie@gmail.com"
 COPY app /app
+COPY data /data
 WORKDIR /app
 RUN pip install -r requirements.txt
 ENTRYPOINT ["python"]
--- app/app.py
+++ app/app.py
@@ -1,13 +1,37 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, redirect
+from flask import request
+from flask import url_for
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Flask inside Docker!!"
+@app.route('/')
+def root():
+    return redirect(url_for(go.__name__))
+
+
+@app.route("/go", methods=['GET', 'POST'])
+def go():
+    if request.method == 'POST':
+        champion = request.form['champion']
+        set_champion(champion)
+        app.logger.debug("New champion: {}".format(champion))
+    else:
+        champion = get_champion()
+    return render_template('go.html', name=champion)
+
+
+def get_champion():
+    with open('/data/champion', 'r') as _file:
+            champion = _file.read()
+    return champion
+
+
+def set_champion(champion):
+    with open('/data/champion', 'w') as _file:
+        _file.write(champion)
 
 
 if __name__ == "__main__":
